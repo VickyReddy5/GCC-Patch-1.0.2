518e9cbfda23ea70847a45db64182a560f2e6d6a
VickyReddy5 <31163346+VickyReddy5@users.noreply.github.com>
2019-10-14 09:11:50 +0530

    Update Question1.py

--- python/Question1.py
+++ python/Question1.py
@@ -1,5 +1,9 @@
 # modify this function, and create other functions below as you wish
 def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
     # modify and then return the variable below
-    answer = -5
+    time = int(input())
+    answer = initialLevelOfDect
+    for i in range(time):
+        answer=answer+(answer*intrestPercentage)/100
+    answer = repaymentPercentage * answer
     return answer
